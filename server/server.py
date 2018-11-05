442c4405552e3f561af61af241bcab2b828ffb3b
Michael Phantom <atemhacking@gmail.com>
2018-11-05 08:36:55 +0700

    Update server.py

--- server/server.py
+++ server/server.py
@@ -11,6 +11,7 @@ import psutil
 import select
 import pickle
 import struct
+
 from threading import *
 from PyQt5 import uic
 from PyQt5.QtGui import *
@@ -23,6 +24,7 @@ from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_PSS
 
 signal.signal(signal.SIGINT,signal.SIG_DFL)
+signal.signal(signal.SIGPIPE,signal.SIG_DFL)
 
 marshall = pickle.dumps
 unmarshall = pickle.loads
@@ -44,28 +46,33 @@ def kill_proc_tree(pid, including_parent=True):
 	if including_parent:
 		parent.kill()
 
-def send(channel, *args):
-    buf = marshall(args)
-    value = socket.htonl(len(buf))
-    size = struct.pack("L",value)
-    channel.send(size)
-    channel.send(buf)
+def send(channel, *msg):
+	buf = marshall(msg)
+	value = socket.htonl(len(buf))
+	size = struct.pack("L",value)
+	channel.send(size)
+	channel.send(buf)
 
 def receive(channel):
 
-    size = struct.calcsize("L")
-    size = channel.recv(size)
-    try:
-        size = socket.ntohl(struct.unpack("L", size)[0])
-    except struct.error as e:
-        return ''
+	size = struct.calcsize("L")
+	size = channel.recv(size)
+	try:
+		size = socket.ntohl(struct.unpack("L", size)[0])
+	except struct.error as e:
+		return ''
 
-    buf = ""
+	buf = ""
 
-    while len(buf) < size:
-        buf = channel.recv(size - len(buf))
+	while len(buf) < size:
+		buf = channel.recv(size - len(buf))
 
-    return unmarshall(buf)[0]
+	return unmarshall(buf)[0]
+
+class UserMap:
+	def __init__(self,sock,name):
+		self.sock = sock
+		self.name = name
 
 class ServerWindow(QMainWindow):
 	def __init__(self,host="127.0.0.1",port=1100):
@@ -78,8 +85,10 @@ class ServerWindow(QMainWindow):
 		self.STATE = True
 		self.outputs = []
 		self.rooms = {}
-		self.clientmap = {}
+		self.room_map = {}
+		self.client_map = {}
 		self.socket_list = []
+		self.commands = ["-create","-join","-leave"]
 		self.RECV_BUFFER = 4096
 
 		self.setWindowIcon(QIcon(QPixmap(icon)))
@@ -96,15 +105,15 @@ class ServerWindow(QMainWindow):
 		self.tableWidget.horizontalHeader().setSectionResizeMode(1,QHeaderView.Stretch)
 
 	def get_just_name(self, client):
-		return self.clientmap[client][1]
+		return self.client_map[client][1]
 
 	def get_address(self, client):
-		return self.clientmap[client][0]
+		return self.client_map[client][0]
 
 	def send_encrypted(self, to_who, message, name):
 		try:
 			message = message.encode()
-			encryptor = self.clientmap[to_who][2]
+			encryptor = self.client_map[to_who][2]
 			msg = encryptor.encrypt(message,0)
 			send(to_who, msg)
 
@@ -128,11 +137,60 @@ class ServerWindow(QMainWindow):
 
 			self.tableWidget.removeRow(index.row())
 
+	def join_room(self,sock,data):
+		room_name = data.split()[1]
+		user_name = self.get_just_name(sock)
+		host = self.get_address(sock)
+
+		if room_name in self.rooms.keys():
+			self.rooms[room_name].append(user_name)
+			self.room_map[room_name].append(UserMap(sock,user_name))
+
+			self.textEdit.append(formatResult
+					(
+						color="#f99a34",text="User %s@%s:%d joined the #%s room" % (user_name,host[0],host[1],room_name)
+					)
+			)
+
+			for user in self.room_map[room_name]:
+				if user.sock != sock: send(user.sock,"User %s joined to the room" % user_name)
+
+				else: send(user.sock,"Welcome,to the #%s room" % room_name)
+
+			for o in self.client_map.keys(): 
+				send(o,self.rooms)
+
+		else: send(sock,"No such that room !!!")
+
+	def create_room(self,sock,data):
+		room_name = data.split()[1]
+
+		if room_name in self.rooms.keys(): send(sock,"That room is avaiable,you can join in.")
+
+		else:
+			user = self.get_just_name(sock)
+			host = self.get_address(sock)
+
+			self.rooms[room_name] = [user]
+			self.room_map[room_name] = [UserMap(sock,user)]
+
+			for o in self.client_map.keys():
+				send(o,self.rooms)
+				if o != sock:
+					send(o,"New room #%s is avaiable now." % room_name)
+
+			self.textEdit.append(formatResult
+					(
+						color="#f99a34",text="User %s@%s:%d created #%s room" % (user,host[0],host[1],room_name)
+					)
+			)
+
 	def setupServer(self):
 		self.pushButton.setEnabled(False)
 		self.textEdit.insertHtml(formatResult(color="blue",text="Setting up server !"))
 		self.server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 		self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.server.setblocking(0)
 		self.server.bind((self.host, int(self.port)))
 
 		self.textEdit.append(formatResult(color="blue",text="Generating RSA keys..."))
@@ -142,7 +200,7 @@ class ServerWindow(QMainWindow):
 
 		self.textEdit.append(formatResult(color="green",text="Server is ready"))
 
-		self.server.listen(10)
+		self.server.listen(30)
 		self.socket_list.append(self.server)
 		self.textEdit.append(formatResult(color="green",text="Listening for clients..."))
 
@@ -169,64 +227,98 @@ class ServerWindow(QMainWindow):
 						)
 					)
 
-					self.clientmap[client] = (address, cname, pubkey)
+					self.client_map[client] = (address, cname, pubkey)
 					self.updateTableWidget(True,cname,address[0],address[1])
-					
 					send(client,self.rooms)
-
-					msg = "Client (%d) %s entered to the room" % (client.fileno(),self.get_just_name(client))
-
-					for o in self.outputs:
-						try:
-							self.send_encrypted(o, msg, self.get_just_name(o))
-
-						except socket.error:
-							if o in self.outputs:
-								self.outputs.remove(o)
-							if o in self.socket_list:
-								self.socket_list.remove(o)
-
 					self.outputs.append(client)
 
 				else:
 					try:
+						flag = False
 						data = receive(sock)
+
 						if data:
-							dataparts = data.split('#^[[')
-							data = dataparts[0]
-							text = "[{0}] > {1}".format(self.get_just_name(sock),data)
-							for o in self.outputs:
-								if o != sock:
-									self.send_encrypted(o,text,self.get_just_name(sock))
+							if self.rooms == {}:
+								if data.split()[0] not in self.commands:
+									send(sock,"No one hear you at here.Join the rooms!!!")
+									send(sock,"Use -create command to create a new room.")
+									send(sock,"Ex: -create hacking.")
+
+								else:
+									if data.split()[0] == "-join" or data.split()[0] == "-leave":
+										send(sock,"No rooms avaiable to %s" % data.split()[0])
+
+									else: self.create_room(sock,data)
+
+							elif self.rooms != {}:
+								user_name = self.get_just_name(sock)
+								for room in self.room_map:
+									for user in self.room_map[room]:
+										if user.sock == sock and user.name == user_name:
+											room_name = room
+											flag = True
+
+								if not flag:
+									if data.split()[0] not in self.commands:
+										send(sock,"You was not joining any rooms,please join the room to chat!!!")
+										send(sock,"Ex: -join room_name")
+
+									elif data.split()[0] == "-leave": send(sock,"You was not joining,so you can't leave.")
+
+									elif data.split()[0] == "-create": self.create_room(sock,data)
+
+									elif data.split()[0] == "-join": self.join_room(sock,data)
+
+								else:
+									dataparts = data.split('#^[[')
+									data = dataparts[0]
+									text = "[{0}] > {1}".format(user_name,data)
+
+									for o in self.room_map[room_name]:
+										if o.sock != sock:
+											self.send_encrypted(o.sock,text,user_name)
+
 						else:
-
-							msg = "Client %d (%s) is offline" % (sock.fileno(),self.get_just_name(sock))
-
-							self.socket_list.remove(sock)
 							self.outputs.remove(sock)
+							address = self.get_address(sock)
+							user_name = self.get_just_name(sock)
+							msg = "User %d (%s) is offline" % (sock.fileno(),user_name)
 
-							for o in self.outputs:
-								self.send_encrypted(o,msg,self.get_just_name(o))
+							for room in self.rooms:
+								if user_name in self.rooms[room]:
+									self.rooms[room].remove(user_name)
+									for user in self.room_map[room]:
+										if user.name == user_name and user.sock == sock:
+											self.room_map[room].remove(user)
 
-							if sock in self.socket_list:
-								self.socket_list.remove(sock)
+										self.send_encrypted(user.sock,msg,user_name)
+
+							if sock in self.socket_list: self.socket_list.remove(sock)
+
+							if sock in self.client_map: del self.client_map[sock]
+
+							for o in self.outputs: 
+								send(o,self.rooms)
+
 							self.textEdit.append(formatResult
 								(
-									color="red",text="Client %s %s disconnected" % (self.get_just_name(sock),self.get_address(sock))
+									color="red",text="Client %s %s disconnected" % (user_name,address)
 								)
 							)
-							address = self.get_address(sock)
-							self.updateTableWidget(False,self.get_just_name(sock),address[0],address[1])
+							
+							self.updateTableWidget(False,user_name,address[0],address[1])
 					except:
-						self.socket_list.remove(sock)
 						self.outputs.remove(sock)
-			time.sleep(0.1)
+						self.socket_list.remove(sock)
+						
+			time.sleep(.1)
 			QCoreApplication.processEvents()
 		self.server.close()
 		
 	def close_server(self):
 		try:
 			self.thread_event.set()
+			self.server.shutdown(SHUT_RDWR)
 			self.server.close()
 		except : pass
 		self.close()
